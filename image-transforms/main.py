d0be9cfbe0091a38f1cd057962b9d4892d02d63f
Ashley Grealish <ashley@grealish.co.uk>
2018-05-03 08:43:47 +0100

    Adding basic image-transform service and docker-compose

---
+++ image-transforms/main.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify, send_file
+from PIL import Image, ImageFilter
+from io import BytesIO
+
+app = Flask(__name__)
+
+TRANSFORM = 'blur'
+
+@app.route("/transform", methods=['POST'])
+def transform_image():
+    if request.method == 'POST':
+
+        # Get the image to be transformed
+        image = Image.open(request.files['image'])
+
+        # Perform the transform
+        if TRANSFORM == 'rotate':
+            image = image.rotate(180)
+        elif TRANSFORM == 'thumbnail':
+            iamge = image.thumbnail((64, 64))
+        elif TRANSFORM == 'compress':
+            image = image
+        elif TRANSFORM == 'blur':
+            image = image.filter(ImageFilter.GaussianBlur(10))
+
+        # Return the transformed image from memory
+        mem_file = BytesIO()
+        image.save(mem_file, "JPEG")
+        return mem_file.getvalue(), 200, [('Content-Type', 'image/jpeg')]
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
